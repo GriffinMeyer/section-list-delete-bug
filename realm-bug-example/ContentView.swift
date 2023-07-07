8d7816d4fe07f3102cb840bfb22a5b70898f01cb
Griffin Meyer <griffin@jaquishbiomedical.com>
2023-07-07 16:54:05 -0500

    Add code

--- realm-bug-example/ContentView.swift
+++ realm-bug-example/ContentView.swift
@@ -6,14 +6,56 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct ContentView: View {
+    @ObservedSectionedResults(Todo.self, sectionKeyPath: \.status) var todos
+    
+    func delete(at offsets: IndexSet, section: ResultsSection<String, Todo>){
+        let deleteList = offsets.map { index in
+            section[index]
+        }
+        let realm = try! Realm()
+        try! realm.write {
+            for todo in deleteList {
+                realm.delete(todo)
+            }
+            
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List {
+                ForEach(todos) { section in
+                    Section(header: Text(section.key)) {
+                        ForEach(section) { todo in
+                            Text(todo.name)
+                        }
+                        .onDelete{
+                            delete(at: $0, section: section)
+                        }
+                    }
+                    
+                }
+            }
+            Button("Add Ready Todo"){
+                let realm = try! Realm()
+                try! realm.write {
+                    let todo = Todo(name: "New", ownerId: "123")
+                    todo.status = "ready"
+                    realm.add(todo)
+                }
+            }
+            
+            Button("Add Blocked Todo"){
+                let realm = try! Realm()
+                try! realm.write {
+                    let todo = Todo(name: "New", ownerId: "123")
+                    todo.status = "Blocked"
+                    realm.add(todo)
+                }
+            }
         }
         .padding()
     }
--- realm-bug-example/Todo.swift
+++ realm-bug-example/Todo.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import RealmSwift
+
+class Todo: Object, ObjectKeyIdentifiable {
+   @Persisted(primaryKey: true) var _id: ObjectId
+   @Persisted var name: String = ""
+   @Persisted var status: String = ""
+   @Persisted var ownerId: String
+   convenience init(name: String, ownerId: String) {
+       self.init()
+       self.name = name
+       self.ownerId = ownerId
+   }
+}
