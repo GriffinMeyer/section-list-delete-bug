6bc75dbef62c42f19b93b2982c7e0810f5166969
Griffin Meyer <griffin@jaquishbiomedical.com>
2023-07-07 14:58:05 -0500

    Initial Commit

---
+++ realm-bug-example/realm_bug_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  realm_bug_exampleApp.swift
+//  realm-bug-example
+//
+//  Created by Griffin Meyer on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct realm_bug_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
